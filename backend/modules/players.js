a7f781a8a013a3d00a8852374fc60e137d159d69
Alartan <alartatrala@interia.pl>
2021-08-09 17:16:16 +0200

    Players - players: private, error and tests

--- backend/modules/players.js
+++ backend/modules/players.js
@@ -1,31 +1,41 @@
 import Player from "./player.js";
 
-export default class Players {
+export default class players {
+  #players;
+
   constructor() {
-    this.players = {};
+    this.#players = {};
   }
 
   addPlayer = (name) => {
-    this.players[name] = new Player(name);
+    this.#players[name] = new Player(name);
   }
 
   getPlayer(name) {
-    return {
-      score: this.players[name].getScore(),
-      travel: player.travel // change it for travel
-    };
+    const player = this.#players[name];
+    try {
+      if (player)
+      return {
+        score: player.getScore(),
+        travel: player.travel // change it for travel
+      };
+      throw new Error(`No player with name ${name}`);
+    } catch (e) {
+      console.error('players', e.message);
+      throw e;
+    }
   }
 
   incrementScore(name) {
-    this.players[name].incrementScore();
+    this.#players[name].incrementScore();
   }
 
   showTile(name, tile) {
-    this.players[name].showTile(tile);
+    this.#players[name].showTile(tile);
   }
 
   isWin(name) {
-    return this.players[name].travel.reduce((sum, e) => {
+    return this.#players[name].travel.reduce((sum, e) => {
       if (e === 'T') sum++;
       return sum;
     }, 0) === 3 ? true : false;
--- backend/tests/players.test.js
+++ backend/tests/players.test.js
@@ -1,6 +1,59 @@
 import Players from '../modules/players';
 
 test('New players', () => {
-  let test_player = new Players;
-  expect(test_player.players).toStrictEqual({});
+  let players = new Players;
+  expect(() => { players.getPlayer('test'); }).toThrow(`No player with name test`);
+});
+
+test('New players added', () => {
+  let players = new Players;
+  players.addPlayer("test1");
+  expect(players.getPlayer("test1")).
+    toStrictEqual({
+      "score": 0,
+      "travel": [
+        " ", " ", " ", " ", " ",
+        " ", " ", " ", " ", " ",
+        " ", " ", " ", " ", " ",
+        " ", " ", " ", " ", " ",
+        " ", " ", " ", " ", " "]
+    });
+});
+
+test('New players increment score', () => {
+  let players = new Players;
+  players.addPlayer("test1");
+  players.addPlayer("test2");
+  players.incrementScore("test1");
+  expect(players.getPlayer("test1")).
+    toStrictEqual({
+      "score": 1,
+      "travel": [
+        " ", " ", " ", " ", " ",
+        " ", " ", " ", " ", " ",
+        " ", " ", " ", " ", " ",
+        " ", " ", " ", " ", " ",
+        " ", " ", " ", " ", " "]
+    });
+    expect(players.getPlayer("test2")).
+      toStrictEqual({
+        "score": 0,
+        "travel": [
+          " ", " ", " ", " ", " ",
+          " ", " ", " ", " ", " ",
+          " ", " ", " ", " ", " ",
+          " ", " ", " ", " ", " ",
+          " ", " ", " ", " ", " "]
+      });
+    [...Array(8)].forEach(() => players.incrementScore("test1"));
+    expect(players.getPlayer("test1")).
+      toStrictEqual({
+        "score": 9,
+        "travel": [
+          " ", " ", " ", " ", " ",
+          " ", " ", " ", " ", " ",
+          " ", " ", " ", " ", " ",
+          " ", " ", " ", " ", " ",
+          " ", " ", " ", " ", " "]
+      });
 });
